493f0a754fa94e1316c88227bc7829a9ae34f492
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2021-06-11 12:01:48 +0100

    <rearrange>[modified folder structure to hold multiple versions]

--- setup_ifort.sh
+++ setup_ifort.sh
@@ -1,2 +1,2 @@
 . /opt/intel/oneapi/setvars.sh
-LD_LIBRARY_PATH=/opt/intel/oneapi/compiler/2021.2.0/linux/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/intel/oneapi/compiler/2021.2.0/linux/lib/:$LD_LIBRARY_PATH
